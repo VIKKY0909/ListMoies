bf25f7947c1a0189ac5cd2de0b6dd776a90c37d7
vlink <vlinkenstarks@gmail.com>
2024-03-22 18:27:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect,useState } from "react";
+import './app.css';
+import SearchIcon from './search.svg';
+import Moviecard from "./Moviecard";
+// 884c45d9
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiKey = "884c45d9"; // Consider using a more secure environment variable for the API key
+const baseUrl = `http://www.omdbapi.com?apikey=${apiKey}`
+const movie={
+    "Title": "Lauf um Dein Leben - Vom Junkie zum Ironman",
+    "Year": "2008",
+    "imdbID": "tt0954542",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDJhZjA5MWEtOTE5Yy00MWJiLTgwNjQtMDliOWI0NWJmZDZkXkEyXkFqcGdeQXVyMjY1ODY2Ng@@._V1_SX300.jpg"
 }
 
+const App = () =>{
+
+    const[movies,setMovies] =useState([]);
+    const[searchterm,setSearchterm] =useState('');
+    const serch= async(title) =>{
+        const response = await fetch(`${baseUrl}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+    useEffect(()=>{
+        serch();
+    },[])
+    return(   
+    
+    <div className="app">
+        <h1>My movies</h1>
+
+
+        <div className="search">
+            <input 
+            placeholder="Search for your movies"
+            value={searchterm}
+            onChange={(e)=>setSearchterm(e.target.value)}
+            ></input>
+            <img 
+            src={SearchIcon}
+            alt="Search"
+            onClick={()=> serch(searchterm)}
+            />
+        </div>
+        {
+            movies?.length>0
+            ? (
+                <div className="container">
+                    {movies.map((movie)=>(
+                        
+                        
+                    <Moviecard movie={movie}/>
+                    ))}
+                </div>
+            ):(
+                <div className="empty">
+                    <h2>Not Found</h2>
+                </div>
+            )
+
+        }
+        
+
+    </div>
+    
+    );
+}
 export default App;
